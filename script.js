ef7f015332c2865d626f2b2b4f4de3ecd28289ab
NagarajR1896 <r.nagaraj0811@gmail.com>
2024-05-29 19:46:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 
 
-/*document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Message Sent!<br>Thanks for completing the form. We','ll be in touch soon!');
-            
-        });
-*/
 $(document).ready(function(){
     $("#contact-form").submit(function(event){
         event.preventDefault();
-        alert('Message sent');
+        alert("Message sent!\nThanks for completing the form. We'll be in touch soon!");
+        this.reset();
     })
 })
